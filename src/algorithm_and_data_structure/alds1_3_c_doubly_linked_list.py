c5034e54c7a0ea9acf6c33ffaa0d3238a8cde110
Gen <archiherewego@gmail.com>
2019-12-19 20:17:46 +0900

    Solve linked list using deque
    結局自前実装ではtimeoutの壁を超えられなかった

--- src/algorithm_and_data_structure/alds1_3_c_doubly_linked_list.py
+++ src/algorithm_and_data_structure/alds1_3_c_doubly_linked_list.py
@@ -1,84 +1,27 @@
 # 連結リスト P95
 # http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_3_C&lang=jp
+from collections import deque
 
 
-class LinkedList:
-    def __init__(self):
-        self.__end = LinkedElement()
-        self.__end.next = self.__end
-        self.__end.prev = self.__end
-        self.__current = self.__end.next
-
-    def insert(self, key):
-        new = LinkedElement(key)
-        old_first = self.__end.next
-        old_first.prev = new
-        new.next = old_first
-        new.prev = self.__end
-        self.__end.next = new
-
-    def delete(self, key):
-        for e in self:
-            if e.value == key:
-                e.prev.next = e.next
-                e.next.prev = e.prev
-                return
-
-    def delete_first(self):
-        old_first = self.__end.next
-        new_first = old_first.next
-        new_first.prev = self.__end
-        self.__end.next = new_first
-
-    def delete_last(self):
-        old_last = self.__end.prev
-        new_last = old_last.prev
-        new_last.next = self.__end
-        self.__end.prev = new_last
-
-    def __iter__(self):
-        self.__current = self.__end.next
-        return self
-
-    def __next__(self):
-        if self.__current == self.__end:
-            raise StopIteration
-        cur = self.__current
-        self.__current = self.__current.next
-        return cur
-
-    def dump(self):
-        return " ".join([str(e.value) for e in self])
-
-    def __str__(self):
-        return "LinkedList<[%s]>(end:%s)" % (self.dump(), self.__end)
-
-
-class LinkedElement:
-    def __init__(self, value=-1, prev=None, next=None):
-        self.value = value
-        self.prev = prev
-        self.next = next
-
-    def __str__(self):
-        return "LinkedElement<%s← %d →%s>" % (self.prev, self.value, self.next)
-
-
-def do_code():
-    list = LinkedList()
+# TODO: 余裕があったら自前実装もやる、ただあんまり意味ないかもなぁ
+def main():
+    linked_list = deque()
     num = int(input())
     for _ in range(num):
-        command = input().split()
+        command = input().split(" ")
         if command[0] == "insert":
-            list.insert(int(command[1]))
+            linked_list.appendleft(command[1])
         elif command[0] == "delete":
-            list.delete(int(command[1]))
+            try:
+                linked_list.remove(command[1])
+            except ValueError:
+                continue
         elif command[0] == "deleteFirst":
-            list.delete_first()
+            linked_list.popleft()
         else:
-            list.delete_last()
-    print(list.dump())
+            linked_list.pop()
+    print(" ".join(linked_list))
 
 
 if __name__ == '__main__':
-    do_code()
+    main()
--- tests/algorithm_and_data_structure/test_alds1_3_c_doubly_linked_list.py
+++ tests/algorithm_and_data_structure/test_alds1_3_c_doubly_linked_list.py
@@ -1,20 +1,32 @@
 from tests.si_so_test_case import SISOTestCase
-from algorithm_and_data_structure.alds1_3_c_doubly_linked_list import do_code
+from algorithm_and_data_structure.alds1_3_c_doubly_linked_list import main
 
 
 class Test(SISOTestCase):
     def test_first_case(self):
         self.helper("7\ninsert 5\ninsert 2\ninsert 3\ninsert 1\ndelete 3\ninsert 6\ndelete 5",
-                    "6 1 2\n", do_code)
+                    "6 1 2\n", main)
 
     def test_second_case(self):
         self.helper("9\ninsert 5\ninsert 2\ninsert 3\ninsert 1\ndelete 3\ninsert 6\ndelete 5\ndeleteFirst\ndeleteLast",
-                    "1\n", do_code)
+                    "1\n", main)
 
     def test_third_case(self):
         self.helper("21\ninsert 1\ninsert 2\ninsert 3\ninsert 4\ninsert 5\ninsert 6\ninsert 7\ninsert 8\ndelete 5\ndelete 7\ninsert 9\ndeleteLast\ndeleteLast\ninsert 10\ndeleteFirst\ndeleteFirst\ndelete 8\ninsert 7\ninsert 8\ndelete 4\ninsert 1",
-                    "1 8 7 6 3\n", do_code)
+                    "1 8 7 6 3\n", main)
 
     def test_fourth_case(self):
         self.helper("5\ninsert 7\ninsert 3\ndeleteFirst\ndeleteLast\ninsert 1",
-                    "1\n", do_code)
+                    "1\n", main)
+
+    def test_fifth_case(self):
+        self.helper("""8
+insert 1000000000
+insert 999999999
+deleteLast
+insert 1234566890
+insert 5
+deleteFirst
+insert 7
+delete 5""",
+                    "7 1234566890 999999999\n", main)
