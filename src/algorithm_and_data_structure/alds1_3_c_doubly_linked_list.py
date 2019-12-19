543a98c608ff86a0d9eb34413f2b529e0e79d76a
Gen <archiherewego@gmail.com>
2019-12-19 16:07:28 +0900

    Refine linked list
    番兵を配置(先頭・末尾を表すNode)を使うことでロジックを簡略化できる
    ただしこの状態だとまだtimeout

--- src/algorithm_and_data_structure/alds1_3_c_doubly_linked_list.py
+++ src/algorithm_and_data_structure/alds1_3_c_doubly_linked_list.py
@@ -4,99 +4,64 @@
 
 class LinkedList:
     def __init__(self):
-        self.__size = 0
-        self.__first = None
-        self.__current = self.__first
-        self.__last = self.__first
+        self.__end = LinkedElement()
+        self.__end.next = self.__end
+        self.__end.prev = self.__end
+        self.__current = self.__end.next
 
-    def command(self, command, key=""):
-        if command == "insert":
-            # do insert
-            element = LinkedElement(int(key))
-            old_first = self.__first
-            element.connect_next(old_first)
-            if old_first is not None:
-                old_first.connect_prev(element)
-            self.__first = element
-            if self.__size == 0:
-                self.__last = element
-            self.__size += 1
-        elif command == "delete":
-            target = int(key)
-            for e in self:
-                if e.value == target:
-                    prev = e.prev()
-                    next = e.next()
-                    if prev is not None:
-                        prev.connect_next(next)
-                    if next is not None:
-                        next.connect_prev(prev)
-                    if e == self.__last:
-                        self.__last = prev
-                    if e == self.__first:
-                        self.__first = next
-                    self.__size -= 1
-                    self.__check_empty()
-                    return
-        elif command == "deleteFirst":
-            self.__first = self.__first.next()
-            if self.__first is not None:
-                self.__first.connect_prev(None)
-            self.__size -= 1
-            self.__check_empty()
-        elif command == "deleteLast":
-            self.__last = self.__last.prev()
-            if self.__last is not None:
-                self.__last.connect_next(None)
-            self.__size -= 1
-            self.__check_empty()
-        else:
-            raise NotImplemented
+    def insert(self, key):
+        new = LinkedElement(key)
+        old_first = self.__end.next
+        old_first.prev = new
+        new.next = old_first
+        new.prev = self.__end
+        self.__end.next = new
+
+    def delete(self, key):
+        for e in self:
+            if e.value == key:
+                e.prev.next = e.next
+                e.next.prev = e.prev
+                return
+
+    def delete_first(self):
+        old_first = self.__end.next
+        new_first = old_first.next
+        new_first.prev = self.__end
+        self.__end.next = new_first
+
+    def delete_last(self):
+        old_last = self.__end.prev
+        new_last = old_last.prev
+        new_last.next = self.__end
+        self.__end.prev = new_last
 
     def __iter__(self):
-        self.__current = self.__first
+        self.__current = self.__end.next
         return self
 
     def __next__(self):
-        if self.__current is None:
+        if self.__current == self.__end:
             raise StopIteration
         cur = self.__current
-        self.__current = self.__current.next()
+        self.__current = self.__current.next
         return cur
 
     def dump(self):
-        return " ".join(map(lambda e: str(e.value), self))
+        return " ".join([str(e.value) for e in self])
 
-    def __check_empty(self):
-        if self.__size == 0:
-            self.__first = None
-            self.__last = None
+    def __str__(self):
+        return "LinkedList<[%s]>(end:%s)" % (self.dump(), self.__end)
 
 
 class LinkedElement:
-    def __init__(self, value=-1):
-        self.__value = value
-        self.__prev = None
-        self.__next = None
+    def __init__(self, value=-1, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
 
-    def has_next(self):
-        return self.__next is not None
-
-    def connect_prev(self, element):
-        self.__prev = element
-
-    def connect_next(self, element):
-        self.__next = element
-
-    @property
-    def value(self):
-        return self.__value
-
-    def next(self):
-        return self.__next
-
-    def prev(self):
-        return self.__prev
+    def __str__(self):
+        return "LinkedElement<%s← %d →%s>" % (self.prev, self.value, self.next)
 
 
 def do_code():
@@ -104,10 +69,14 @@ def do_code():
     num = int(input())
     for _ in range(num):
         command = input().split()
-        if len(command) == 1:
-            list.command(command[0])
+        if command[0] == "insert":
+            list.insert(int(command[1]))
+        elif command[0] == "delete":
+            list.delete(int(command[1]))
+        elif command[0] == "deleteFirst":
+            list.delete_first()
         else:
-            list.command(command[0], command[1])
+            list.delete_last()
     print(list.dump())
 
 
