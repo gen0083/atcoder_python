32148d250dbfaa84cb933093371f9c1149148540
Gen <archiherewego@gmail.com>
2019-12-23 22:18:55 +0900

    Refine atcoder148 e
    自力でもう少し+解説みて改良したけどやっぱわからん・・・

--- src/abc148/e_double_factorial.py
+++ src/abc148/e_double_factorial.py
@@ -1,36 +1,28 @@
 # https://atcoder.jp/contests/abc148/tasks/abc148_e
 # TODO: timeoutを乗り越えろ
-
-dp = dict()
-
-
-def f(n):
-    if n < 2:
-        return 1
-    else:
-        if n in dp:
-            return dp[n]
-        else:
-            a = 0
-            if n - 2 in dp:
-                a = n * dp[n - 2]
-            else:
-                a = n * f(n - 2)
-            dp[n] = a
-            return a
+# 二重階乗になるわけで、sympy.factorial2(n)使うといけるっぽい
+# そもそも10で何回割れるかという話なので、二重階乗の値を求めなくとも答えを導き出せる
+# が式の変形を解説されてもさっぱりわからん
+# https://www.youtube.com/watch?v=F2p_e6iKxnk&feature=youtu.be
+# 自力で10で割れる数を数えたらいいんじゃないかというひらめきをえることはできたんだが、
+# 単純に10で割れる数をカウントアップしても回答にはならない
+# 理由は単純で、10だと1つ0がつくけど、100だと2個つくから
+# そして110は1つ増えるので、単純に10で割ればいいわけではない
 
 
 def main():
     num = int(input())
-    for i in range(num % 2, num, 2):
-        f(i)
-    a = f(num)
+    # 奇数の場合は末尾に0が出てこないので0になる
+    if num % 2 == 1:
+        print(0)
+        return
     count = 0
-    for s in str(a)[::-1]:
-        if s == "0":
-            count += 1
-        else:
-            break
+    step = 1
+    p = num
+    while p > 0:
+        count += p // 10 * step
+        p //= 10
+        step += 1
     print(count)
 
 
