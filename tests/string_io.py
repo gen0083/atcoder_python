c8d9bcd2b0830a95815e6cb21a06a244e3100bfd
Gen <archiherewego@gmail.com>
2020-01-13 20:24:31 +0900

    Improve test speed
    文字列の連結を配列に格納して最後にjoinする方式にした
    標準出力への出力が多いテストケースだと、文字列連結だけで実行時間の8割を締めたりしていたので改善した
    こうすることでコード自体のパフォーマンスを調べることに集中できる
    （priority queueのテストケースで2m→20sにこれだけで改善した。ちなみにローカルマシンでのテストが早くなったというだけ）

--- tests/string_io.py
+++ tests/string_io.py
@@ -9,11 +9,11 @@ class StringIO(io.StringIO):
     def __init__(self, value=''):
         value = value.encode('utf8', 'backslashreplace').decode('utf8')
         io.StringIO.__init__(self, value)
-        self.__buffer = ""
+        self.__buffer = []
 
     def write(self, msg):
-        self.__buffer += msg
+        self.__buffer.append(str(msg))
         io.StringIO.write(self, msg.encode('utf8', 'backslashreplace').decode('utf8'))
 
     def getValue(self):
-        return self.__buffer
+        return "".join(self.__buffer)
