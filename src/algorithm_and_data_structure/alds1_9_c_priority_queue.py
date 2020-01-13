1613d951305f23511fa1fc8716a42474986342f9
Gen <archiherewego@gmail.com>
2020-01-13 21:18:46 +0900

    Solve priority queue
    自前実装で15sくらいかかってたのが4sになった
    標準ライブラリはすごいね

--- src/algorithm_and_data_structure/alds1_9_c_priority_queue.py
+++ src/algorithm_and_data_structure/alds1_9_c_priority_queue.py
@@ -1,56 +1,21 @@
 # 優先度付きキュー
 # http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_9_C&lang=jp
 import sys
-
-
-def insert(heap, value):
-    # 末尾に追加して、その親との比較で必要があれば入れ替えを行う
-    # 完全ヒープの構築まではする必要はない
-    heap.append(value)
-    i = len(heap) - 1
-    while i > 1:
-        p = i // 2
-        if heap[p] < heap[i]:
-            heap[0] = heap[p]
-            heap[p] = heap[i]
-            heap[i] = heap[0]
-            i = p
-        else:
-            break
-
-
-def extract(heap):
-    v = heap[1]
-    heap[1] = heap[-1]
-    heap.pop()
-    max_heapify(heap, 1)
-    return v
-
-
-def max_heapify(heap, root_index):
-    left_index = root_index * 2
-    right_index = left_index + 1
-    max_index = root_index
-    size = len(heap)
-    if left_index < size and heap[left_index] > heap[root_index]:
-        max_index = left_index
-    if right_index < size and heap[right_index] > heap[max_index]:
-        max_index = right_index
-    if max_index != root_index:
-        heap[0] = heap[root_index]
-        heap[root_index] = heap[max_index]
-        heap[max_index] = heap[0]
-        max_heapify(heap, max_index)
+import heapq
 
 
 def main():
-    heap = [0]
+    # heapqは0インデックスでヒープアルゴリズムを実装してるので、普通の配列を使えばいい
+    heap = []
     while True:
         command = sys.stdin.readline().split(" ")
         if command[0] == "insert":
-            insert(heap, int(command[1]))
+            # なんでマイナスで入れてるかというと、heapqがmin-heapの実装だから
+            # マイナスで変換してやれば最大の値がマイナスに反転して最小で並んでくれる
+            heapq.heappush(heap, -int(command[1]))
         elif command[0] == "extract\n":
-            print(extract(heap))
+            # この方法を取る場合、popした際に符号の反転を忘れないように
+            print(-heapq.heappop(heap))
         else:
             break
 
