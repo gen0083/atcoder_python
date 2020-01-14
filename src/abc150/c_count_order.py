ff156062d6ea0f55a31e67eca5f95829b0d3c874
Gen <archiherewego@gmail.com>
2020-01-14 22:08:52 +0900

    Solve abc150 c
    数列を作ればいいってのは当時も思いついたけど、そのやり方が分からなくてその道は諦めて別の方法でやろうとした
    つまり、配列の0から順番に見ていって、辞書順で言えば何番目に該当するかを計算で出すという考え方
    ただこれは、最初の0番目はそれでいいけど、1番めから前に出現した数自分ずれるというのがうまく落とし込めずにできなかった
    しかしこの数列の生成、もっといい方法ないのかな
    C++だとなんか使えばいいとか解説にはあったが

--- src/abc150/c_count_order.py
+++ src/abc150/c_count_order.py
@@ -1,20 +1,35 @@
 # https://atcoder.jp/contests/abc150/tasks/abc150_c
-# TODO: try
+
+
+def generate_pattern(pattern, current, n):
+    c = current[:]
+    remain = n - len(c)
+    if remain > 0:
+        for i in range(1, n + 1):
+            if i not in c:
+                c.append(i)
+                generate_pattern(pattern, c, n)
+                c.pop()
+    else:
+        pattern.append(c)
 
 
 def main():
     n = int(input())
     array_a = [int(s) for s in input().split(" ")]
     array_b = [int(s) for s in input().split(" ")]
-    if array_a == array_b:
-        print("0")
-        return
-    a = 0
-    b = 0
+    pattern = []
+    a = -1
+    b = -1
     for i in range(n):
-        a += get_num(array_a, i, n)
-        b += get_num(array_b, i, n)
-
+        generate_pattern(pattern, [i + 1], n)
+    for i, v in enumerate(pattern):
+        if array_a == v:
+            a = i
+        if array_b == v:
+            b = i
+        if a != -1 and b != -1:
+            break
     print(abs(a - b))
 
 
