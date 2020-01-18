69a2391e95236044f81437251c73fd7e0e7849d1
Gen <archiherewego@gmail.com>
2020-01-18 17:55:35 +0900

    Solve longest common subsequence
    メモ領域はX*Yもなくていい
    ループ中に参照するのは直前の列のみであり、最終結果としては最終行しか使わないので、Yの分だけメモ領域をとることにすればメモリ節約できる
    さらに配列へのアクセスも減るからなのか、pythonだと高速化にもつながる

--- src/algorithm_and_data_structure/alds1_10_c_longest_common_subsequence.py
+++ src/algorithm_and_data_structure/alds1_10_c_longest_common_subsequence.py
@@ -8,16 +8,17 @@ def main():
     for _ in range(q):
         x = sys.stdin.readline().strip()
         y = sys.stdin.readline().strip()
-        dp = [[0 for _ in range(len(y) + 1)] for _ in range(len(x) + 1)]
+        dp = [0 for _ in range(len(y) + 1)]
         for i, cx in enumerate(x, 1):
+            prev = dp[:]
             for j, cy in enumerate(y, 1):
                 if cx == cy:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-                elif dp[i - 1][j] >= dp[i][j - 1]:
-                    dp[i][j] = dp[i - 1][j]
+                    dp[j] = prev[j - 1] + 1
+                elif dp[j - 1] > prev[j]:
+                    dp[j] = dp[j - 1]
                 else:
-                    dp[i][j] = dp[i][j - 1]
-        print(dp[len(x)][len(y)])
+                    dp[j] = prev[j]
+        print(dp[-1])
 
 
 if __name__ == '__main__':
