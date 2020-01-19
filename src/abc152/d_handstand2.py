f7f7d6b1409c3ef766876a7d6520f174da6b243c
Gen <archiherewego@gmail.com>
2020-01-20 00:42:36 +0900

    Solve abc152 problem D
    解説見てだけどね
    要はnまでの数値を先頭の桁と末尾の桁の要素に単純化してそれぞれカウント
    再度ループ回して今度は先頭と末尾を逆にしたものを使って、カウントした分を足し合わせる・・・ということなんだが、これでなんで答えになるのか理解しきれてない

--- src/abc152/d_handstand2.py
+++ src/abc152/d_handstand2.py
@@ -1,11 +1,19 @@
 def main():
     n = int(input())
     count = 0
+    m = dict()
     for i in range(1, n + 1):
-        p = i // 10
         tmp = str(i)
-        if tmp[0] == tmp[-1]:
-            count += (2 * p) + 1
+        t = (tmp[0], tmp[-1])
+        if t in m:
+            m[t] += 1
+        else:
+            m[t] = 1
+    for i in range(1, n + 1):
+        tmp = str(i)
+        t = (tmp[-1], tmp[0])
+        if t in m:
+            count += m[t]
     print(count)
 
 
