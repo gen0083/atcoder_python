0323250ec8d60aab7e32888046fd7110a523c158
Gen <archiherewego@gmail.com>
2020-01-27 23:17:14 +0900

    Solve abc153 problem e
    思いっきり解説のコード丸写しである
    Python3ではTLE、Pypy3でAC

--- src/abc153/e_crested_lbis_vs_monster.py
+++ src/abc153/e_crested_lbis_vs_monster.py
@@ -1,35 +1,17 @@
 # https://atcoder.jp/contests/abc153/tasks/abc153_e
 
 
-class Magic(object):
-    def __init__(self, damage, cost):
-        self.damage = damage
-        self.cost = cost
-        self.efficiency = damage / cost
-
-    def __str__(self):
-        return "Magic(d=%d, c=%d, e=%f)" % (self.damage, self.cost, self.efficiency)
-
-
 def main():
     h, n = map(int, input().split(" "))
-    magic = []
-    for _ in range(n):
-        d, c = map(int, input().split(" "))
-        magic.append(Magic(d, c))
-    # これだとsample3に対応できない
-    magic.sort(key=lambda t: t.efficiency, reverse=True)
-    base = h // magic[0].damage
-    if base * magic[0].damage == h:
-        print(base * magic[0].cost)
-        return
-    remain = h - base * magic[0].damage
-    minimum = magic[0].cost
+    dp = [float("inf")] * (h + 1)
+    dp[0] = 0
     for i in range(n):
-        if magic[i].damage > remain:
-            if minimum > magic[i].cost:
-                minimum = magic[i].cost
-    print(magic[0].cost * base + minimum)
+        damage, cost = map(int, input().split(" "))
+        for total in range(h +1):
+            # 現在の蓄積ダメージから、魔法iを使った場合に与えるダメージ
+            next_damage = min(total + damage, h)
+            dp[next_damage] = min(dp[next_damage], dp[total] + cost)
+    print(dp[-1])
 
 
 if __name__ == '__main__':
