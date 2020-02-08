d152e44a5239e72fa3f274c7e6598c8762148ff9
Gen <archiherewego@gmail.com>
2020-02-08 11:58:41 +0900

    Update single source shortest path1
    ダイクストラは要するにこういう考え方・・・ってことであってるかな
    一応コードはパスしたんで大丈夫っぽいと思いたい
    辺の重みにマイナスがある場合には使えない
    最小全域木に所属させたあとでさらに到達コストが小さくなる可能性があるから

--- src/algorithm_and_data_structure/alds1_12_b_single_source_shortest_path1.py
+++ src/algorithm_and_data_structure/alds1_12_b_single_source_shortest_path1.py
@@ -6,23 +6,31 @@ def main():
     n = int(input())
     data = [[] for _ in range(n)]
     destination = [float("inf") for _ in range(n)]
+    destination[0] = 0
     for _ in range(n):
         i, k, *v = map(int, input().split(" "))
         array = [(v[n * 2], v[(n * 2) + 1]) for n in range(k)]
         data[i] = array
-    step = 0
-    destination[0] = 0
-    shortest_path(0, step, data, destination)
+    mst = {0}
+    while len(mst) < n:
+        # mstの周りの頂点に到達するまでのコストでもっとも小さいものを記憶する変数
+        mini = (0, float("inf"))
+        for i in mst:
+            # mstに属する頂点から到達できる周囲の頂点について到達コストを更新する
+            for t, c in data[i]:
+                cost = destination[i] + c
+                # 移動先がすでにmstに属しているならスキップ
+                if t in mst:
+                    continue
+                # 到達コストが小さくなるなら更新する
+                if destination[t] > cost:
+                    destination[t] = cost
+                # 周囲の頂点のうち到達コストがもっとも小さいものを新たにmstに追加したいので最小コストの頂点を記録
+                if mini[1] > cost:
+                    mini = (t, c)
+        mst.add(mini[0])
     for i, d in enumerate(destination):
         print("%d %d" % (i, d))
-
-
-def shortest_path(start, step, data, destination):
-    for t, c in data[start]:
-        cost = step + c
-        if destination[t] > cost:
-            destination[t] = cost
-            shortest_path(t, cost, data, destination)
 
 
 if __name__ == '__main__':
