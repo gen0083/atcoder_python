2dacd555c229f7c68d91030c8ec311999034e843
Gen <archiherewego@gmail.com>
2020-02-09 20:30:40 +0900

    Update DSL 1 A disjoint set
    Setでも間に合ったが、こちらの解説のアルゴリズムの方が高速
    （自前のが6sギリギリに対して、こちらは700ms）
    愚直にSetとして集合を表現しなくとも、それぞれの要素がrootに対するポインタとして機能するようなツリー構造と考えれば、rootが同じなら同じ集合と判定できる
    そしてrootを判定する過程で経路圧縮もできる（親を示している状態から根を示すように更新する）

--- src/algorithm_and_data_structure/dsl_1_a_disjoint_set_union_find_tree.py
+++ src/algorithm_and_data_structure/dsl_1_a_disjoint_set_union_find_tree.py
@@ -3,28 +3,53 @@
 import sys
 
 
+class DisjointSet(object):
+    def __init__(self, size):
+        self.data = [i for i in range(size)]
+        self.rank = [0 for _ in range(size)]
+
+    def find_root(self, x):
+        # rootノードまでたどる最中に、各要素がrootを指し示すように更新することで経路を短縮する
+        p = x
+        while self.data[p] != p:
+            p = self.data[p]
+            self.data[x] = p
+        self.rank[x] = 0
+        return p
+
+    def union(self, x, y):
+        root_x = self.find_root(x)
+        root_y = self.find_root(y)
+        # rank, ツリーの高さを表す
+        # 低いツリーをrootに据えたほうが効率がよくなるのでどちらを親としてつなげるかを判定している
+        # （高さが高いほど、rootにたどり着くまでの計算量が増えるため、ツリーの高さは低く保ちたい）
+        if self.rank[root_x] <= self.rank[root_y]:
+            self.rank[root_y] += 1
+            self.data[root_y] = root_x
+        else:
+            self.rank[root_x] += 1
+            self.data[root_x] = root_y
+
+    def same(self, x, y):
+        root_x = self.find_root(x)
+        root_y = self.find_root(y)
+        if root_x == root_y:
+            print("1")
+        else:
+            print("0")
+
+
 def main():
     n, q = map(int, sys.stdin.readline().strip().split(" "))
-    data = [{i} for i in range(n)]
+    disjoint_set = DisjointSet(n)
     for _ in range(q):
         command, x, y = map(int, sys.stdin.readline().strip().split(" "))
         if command == 0:
             # union(x, y)
-            entry_a = data[x]
-            entry_b = data[y]
-            if entry_a == entry_b:
-                continue
-            new_entry = entry_a.union(entry_b)
-            for a in entry_a:
-                data[a] = new_entry
-            for b in entry_b:
-                data[b] = new_entry
+            disjoint_set.union(x, y)
         else:
             # same(x, y)
-            if data[x] == data[y]:
-                print("1")
-            else:
-                print("0")
+            disjoint_set.same(x, y)
 
 
 if __name__ == '__main__':
