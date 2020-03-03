273da7cce135ac819084d16e709fac237d1e84f1
Gen <archiherewego@gmail.com>
2020-03-03 20:26:56 +0900

    Re-try ABC157 problem D and not solved
    友人関係で辿れる人のつながりをクラスタとする
    クラスタに含まれる人数と、クラスタ内における友人・ブロックの出自数がわかれば友人候補の数が出る
    しかし友人関係はクラスタをたどるのに必要だが、ブロックはクラスタを調べるのには使わない
    一方で出自数を求めるのにクラスタサイズが必要
    これが難しい
    出自数を求める際に、ブロックはクラスタをまたぐものはカウントしないからややこしい

--- src/abc157/d_friend_suggestions.py
+++ src/abc157/d_friend_suggestions.py
@@ -1,18 +1,36 @@
 import sys
+from collections import deque
 
 
 def main():
     n, m, k = map(int, input().split(" "))
-    array = [0] * n
-    for _ in range(m):
+    cluster = [set() for _ in range(n)]
+    count = [0] * n
+    for _ in range(m + k):
         i, j = map(int, sys.stdin.readline().strip().split(" "))
-        array[i - 1] += 1
-        array[j - 1] += 1
-    for _ in range(k):
-        i, j = map(int, sys.stdin.readline().strip().split(" "))
-        array[i - 1] += 1
-        array[j - 1] += 1
-    ans = map(lambda i: str(n - 1 - i), array)
+        cluster[i - 1].add(j)
+        cluster[j - 1].add(i)
+        count[i - 1] += 1
+        count[j - 1] += 1
+    cluster_size = [-1] * n
+    for i in range(n):
+        if cluster_size[i] != -1:
+            continue
+        stack = deque()
+        stack.append(cluster[i])
+        space = {i + 1}
+        while len(stack) > 0:
+            s = stack.pop()
+            for v in s:
+                if v not in space:
+                    space.add(v)
+                    stack.append(cluster[v - 1])
+        size = len(space)
+        for v in space:
+            cluster_size[v - 1] = size
+    ans = []
+    for i in range(n):
+        ans.append(str(cluster_size[i] - 1 - count[i]))
     print(" ".join(ans))
 
 
