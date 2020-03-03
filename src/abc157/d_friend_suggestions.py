6cce96fcae954da011ec8f65fc6dd32bf32800c6
Gen <archiherewego@gmail.com>
2020-03-03 22:21:57 +0900

    Solve ABC157 problem D
    友人関係とブロック関係を一緒にまとめてやろうとするのがそもそもの間違いだった
    なんとか解説無しで解けた（時間かかり過ぎではあるが）

--- src/abc157/d_friend_suggestions.py
+++ src/abc157/d_friend_suggestions.py
@@ -4,30 +4,36 @@ from collections import deque
 
 def main():
     n, m, k = map(int, input().split(" "))
-    cluster = [set() for _ in range(n)]
+    friends = [set() for _ in range(n)]
+    blocks = [set() for _ in range(n)]
     count = [0] * n
-    for _ in range(m + k):
+    for _ in range(m):
         i, j = map(int, sys.stdin.readline().strip().split(" "))
-        cluster[i - 1].add(j)
-        cluster[j - 1].add(i)
+        friends[i - 1].add(j)
+        friends[j - 1].add(i)
         count[i - 1] += 1
         count[j - 1] += 1
+    for _ in range(k):
+        i, j = map(int, sys.stdin.readline().strip().split(" "))
+        blocks[i - 1].add(j)
+        blocks[j - 1].add(i)
     cluster_size = [-1] * n
     for i in range(n):
         if cluster_size[i] != -1:
             continue
         stack = deque()
-        stack.append(cluster[i])
+        stack.append(friends[i])
         space = {i + 1}
         while len(stack) > 0:
             s = stack.pop()
             for v in s:
                 if v not in space:
                     space.add(v)
-                    stack.append(cluster[v - 1])
+                    stack.append(friends[v - 1])
         size = len(space)
         for v in space:
-            cluster_size[v - 1] = size
+            block_in_cluster = len(space & blocks[v - 1])
+            cluster_size[v - 1] = size - block_in_cluster
     ans = []
     for i in range(n):
         ans.append(str(cluster_size[i] - 1 - count[i]))
