38ed4149edc0de38838e289d46eb744f9d65d5dc
Gen <archiherewego@gmail.com>
2020-03-14 23:42:24 +0900

    Delete log output
    コード動作確認用のログ出力を削除

--- atcoder.sh
+++ atcoder.sh
@@ -16,7 +16,6 @@ template_for_test="atcoder_template_for_test.txt"
 
 function make_source() {
   local file_name="$target_dir/$1.py"
-  echo "$file_name"
   while IFS= read -r line; do
     echo "$line" >>"$file_name"
   done <"$template_for_source"
@@ -26,7 +25,6 @@ function make_source() {
 function make_test() {
   local file_name="$target_dir/test_$1.py"
   target_file="$1"
-  echo "$file_name"
   while IFS= read -r line; do
     eval "echo \"${line}\"" >>"$file_name"
   done <"$template_for_test"
