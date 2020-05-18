8f5c26d2fa44d41231ef289fdd6a391f6f1addc4
Gen <archiherewego@gmail.com>
2020-05-18 21:23:56 +0900

    Try suggested problems from AtCoder Problems
    はい、組み合わせを求める計算も嫌いです

--- kotlin/src/main/kotlin/problems/agc017a.kt
+++ kotlin/src/main/kotlin/problems/agc017a.kt
@@ -4,46 +4,36 @@ fun main(args: Array<String>) {
 
 fun agc017a() {
     val (n, p) = readLine()!!.split(" ").map { it.toInt() }
-    //    val a = readLine()!!.split(" ").map { it.toInt() % 2 }
+    val a = readLine()!!.split(" ").map { it.toInt() % 2 }
     //    val counts = mutableMapOf(0 to 0, 1 to 0)
     //    generatePattern(0, 0, a, counts)
     //    println(counts[p])
     var evenCount = 0
     var oddCount = 0
-    readLine()!!.split(" ")
-        .stream()
-        .map { it.toInt() % 2 }
-        .forEach {
-            if (it == 0) {
-                evenCount++
-            } else {
-                oddCount++
-            }
+    //    readLine()!!.split(" ")
+    //        .stream()
+    //        .map { it.toInt() % 2 }
+    //        .forEach {
+    //            if (it == 0) {
+    //                evenCount++
+    //            } else {
+    //                oddCount++
+    //            }
+    //        }
+    oddCount = a.count { it == 1 }
+    // なぜそうなるのか理屈がわからない
+    // 奇数が含まれる場合なら、偶数だろうと奇数だろうと2^n-1になるのはなぜ？
+    // 奇数があるならその奇数を選ぶパターンがP=0/1のどちらの場合も1パターン減るからということ？
+    val ans = if (oddCount == 0) {
+        if (p == 0) {
+            Math.pow(2.0, n.toDouble()).toLong()
+        } else {
+            0
         }
-    var oddP = 0L
-    val oddPBase = factorial(oddCount)
-    for (i in 2 - p..oddCount step 2) {
-        val f = factorial(i)
-        oddP += oddPBase / f
-    }
-    val evenP = factorial(evenCount)
-    val ans = if (p == 0) {
-        oddP + oddP * evenP + evenP
     } else {
-        oddP + oddP * evenP
+        Math.pow(2.0, n - 1.0).toLong()
     }
     println(ans)
-}
-
-private var memo: LongArray = LongArray(51) { 0L }.apply { set(1, 1L) }
-
-private fun factorial(n: Int): Long {
-    if (n == 0) return 0
-    if (memo[n] != 0L) return memo[n]
-    val p = factorial(n - 1)
-    val f = p * n
-    memo[n] = f
-    return f
 }
 
 // これでやると組み合わせが2^50で10＾15くらいの計算量になる
