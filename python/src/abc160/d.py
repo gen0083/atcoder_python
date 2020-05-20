9520812d6e7cab78db116460f14d0bee295cdee2
Gen <archiherewego@gmail.com>
2020-05-20 19:40:27 +0900

    Try recommendation problem from AtCoder Problems
    途中で力尽きた

--- python/src/abc160/d.py
+++ python/src/abc160/d.py
@@ -1,13 +1,26 @@
-#
+# https://atcoder.jp/contests/abc160/tasks/abc160_d
 
 
 def main():
     n, x, y = map(int, input().split(" "))
-    a = x
-    b = y - x - 2
-    c = n - y
+    k = [0 for _ in range(n)]
+    steps = [[j - i for i in range(n + 1)] for j in range(n + 1)]
+    break_point = (y - x) // 2 + x
+    even_break = 0
+    if (y - x) % 2 == 0:
+        even_break = 1
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            if j < x or i > y:
+                k[steps[i][j]] += 1
+                continue
+            if i < break_point < j < y:
+                k[steps[i][j] - (y - break_point)] += 1
+                continue
+            k[steps[i][j]] += 1
 
-    print()
+    for i in range(1, n):
+        print(k[i])
 
 
 if __name__ == '__main__':
