7c03451203809c5c64aa87df43b9125e8415169f
Gen <archiherewego@gmail.com>
2020-06-27 01:05:31 +0900

    Retry ABC147C
    全組み合わせ突合
    Nの数が15までなのでこのやり方でもTLEにはならない
    honestの仮定は2進数表記でビットが立ってる人を正直者と仮定
    honestの証言をsetに足し合わせていって、honestとunkindが一部でも交差してたら矛盾した証言ということで、その仮定はNG
    逆に交差していないのであれば、証言によるhonestと仮定によるhonestのセットのサイズが正直者としてありうる人数になる（一切証言に出てこない人も存在しうるだろうし、仮定から導き出される証言で矛盾せず仮定より多くのhonestが導き出される可能性もある）
    全組み合わせを試す、という単純な方法のほうが簡単にできることに思い至った
    このパターン（組み合わせを試すのに効率良いやりかたをやろうとして自滅するパターン）が結構多い

--- python/src/abc147/c_honest_or_unkind2.py
+++ python/src/abc147/c_honest_or_unkind2.py
@@ -20,52 +20,31 @@ def do_code():
         honests[n] = h
         unkinds[n] = u
     count = 0
-    for j in range(1, num + 1):
-        # 1~Nまで順番に1人ずつ正直者であると仮定して、他の組み合わせすべてを試す
-        u = unkinds[j]
-        h = honests[j]
-        contradiction = True
-        # jを除く各人について、正直者であると仮定したときに矛盾が生じるかどうかをチェックする
-        for i in range(1, num + 1):
-            if i == j:
+    for n in range(1, 2 ** num):
+        honest_set = set()
+        for j in range(num):
+            t = 2 ** j
+            if n & t == t:
+                honest_set.add(j + 1)
+        th = set()
+        tu = set()
+        queue = deque(honest_set)
+        visited = set()
+        while len(queue) > 0:
+            t = queue.popleft()
+            if t in visited:
                 continue
-            tu = set()
-            th = set()
-            th.add(i)
-            isOk = True
-            queue = deque(honests[i])
-            visited = {i}
-            while len(queue) > 0:
-                t = queue.popleft()
-                if t in visited:
-                    continue
-                visited.add(t)
-                if t in th or t in h:
-                    continue
-                if t in tu or t in u:
-                    # 現在の仮定と矛盾するため、人物iをhonestと仮定できない
-                    isOk = False
-                    break
-                queue.extend(honests[t])
-                th |= honests[t]
-                tu |= unkinds[t]
-            if not isOk:
-                continue
-            for unkind in unkinds[i]:
-                # iを正直者と仮定するため、unkindと証言している人物はunkindであると仮定する
-                # ただし現在の仮定と矛盾する場合は人物iをhonestとすると矛盾が生じる
-                # unkindについてはunkindと証言されている人物の発言までチェックする必要はない
-                if unkind not in th and unkind not in h:
-                    tu.add(unkind)
-                else:
-                    isOk = False
-                    break
-            if isOk:
-                contradiction = False
-                h |= th
-                u |= tu
-        if not contradiction:
-            count = max(count, len(h))
+            visited.add(t)
+            queue.extend(honests[t])
+            th |= honests[t]
+            tu |= unkinds[t]
+        if not th.isdisjoint(tu):
+            # 正直者と仮定した人の証言を集めた場合にhonest && unkindな人がいる＝矛盾
+            continue
+        if not honest_set.isdisjoint(tu):
+            continue
+        honest = max(len(th), len(honest_set))
+        count = max(count, honest)
     print(count)
 
 
