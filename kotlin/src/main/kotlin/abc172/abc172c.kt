5021e1ee85e4e7e5bd5b435c99b270bdd4faf9e0
Gen <archiherewego@gmail.com>
2020-06-28 18:20:39 +0900

    Retry ABC172C
    最初は一番上に乗っかってる本同士を比較して、早く読める方を優先して読んでいく戦略をとった
    これだとその下に短い時間で読める本が大量に積まれている場合に対応できない
    次にDPを使って読むのに必要な時間を計算する方法を取る
    2次元配列を使ったらメモリ制限に引っかかってRE
    一次元テーブルでのDP（というのか？）で試すと今度は計算量がネックになってTLE
    というわけでようやくACになるコードにたどり着いた

--- kotlin/src/main/kotlin/abc172/abc172c.kt
+++ kotlin/src/main/kotlin/abc172/abc172c.kt
@@ -15,21 +15,25 @@ fun abc172c() {
     val times = LongArray((m + 1).toInt()) { 0L }
     val ni = n.toInt()
     val mi = m.toInt()
+    var max_j = 0
     for (j in 0 until mi) {
         times[j + 1] = times[j] + b[j]
         if (times[j + 1] <= k) {
             max_count = max(max_count, j + 1)
+            max_j = j + 1
         } else {
             break
         }
     }
+    var ai = 0L
     for (i in 1..ni) {
-        for (j in 0..mi) {
-            times[j] += a[i - 1]
-            if (times[j] <= k) {
+        ai += a[i - 1]
+        for (j in max_j downTo 0) {
+            if (times[j] + ai <= k) {
                 max_count = max(max_count, i + j)
-            } else {
                 break
+            } else {
+                max_j = j
             }
         }
     }
