89f4947d291cd0bd6439464272264a64bfc86997
Gen <archiherewego@gmail.com>
2020-07-01 22:03:21 +0900

    Try ABC129C
    DP使えば楽勝って思ったら思いの外ハマった
    最後にmodとればいいと思ったら、途中でオーバーフローしてしまうので毎回modとってやる必要があった

---
+++ kotlin/src/main/kotlin/problems/abc129c.kt
@@ -0,0 +1,24 @@
+fun main() {
+    abc129c()
+}
+
+fun abc129c() {
+    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+    val step = LongArray(n + 1) { 0L }
+    repeat(m) {
+        val i = readLine()!!.toInt()
+        step[i] = -1L
+    }
+    step[0] = 1
+    for (i in 1..n) {
+        if (step[i] == -1L) continue
+        val one = step[i - 1]
+        val two = if (i > 1) step[i - 2] else 0L
+        var s = 0L
+        if (one != -1L) s += one
+        if (two != -1L) s += two
+        // 毎回modとらないとオーバーフローしてWAになる
+        step[i] = s % 1_000_000_007L
+    }
+    println(step[n].coerceAtLeast(0L))
+}
