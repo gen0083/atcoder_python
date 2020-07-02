00494ac1c4cd1230264fe96a7de1c15f5ea193d4
Gen <archiherewego@gmail.com>
2020-07-02 23:05:49 +0900

    Retry Panasonic2020D

--- python/src/panasonic2020/d.py
+++ python/src/panasonic2020/d.py
@@ -3,16 +3,16 @@
 
 def main():
     n = int(input())
-    data = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-    array = [0] * n
-    for i in range(1, n):
-        initial = "a" * (n - i)
-        for j in range(i):
-            tmp = j
-            while tmp > 0:
-                a = tmp % i
-                tmp -= a
-    print("".join(initial[0]))
+    data = [chr(ord("a") + i) for i in range(10)]
+    generate("a", 0, n, data)
+
+
+def generate(cur, max_i, n, data):
+    if len(cur) == n:
+        print(cur)
+        return
+    for i in range(max_i + 2):
+        generate(cur + chr(ord("a") + i), max(max_i, i), n, data)
 
 
 if __name__ == '__main__':
