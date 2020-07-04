38d1f8c21116a330dc44874ed4be922fa2e31373
Gen <archiherewego@gmail.com>
2020-07-04 11:13:52 +0900

    Retry ABC146D
    実装はできるけどスマートにできない
    その場のワーキングメモリでやってるから、時間を開けたり集中が削がれたりするとすぐにミスる
    今回はさらに、すでに使われている色をのけて数字を割り当てる、という作業が必要なので余計に混乱した
    解法はともかく、それを実装するのが大変だった

--- kotlin/src/main/kotlin/problems/abc146d.kt
+++ kotlin/src/main/kotlin/problems/abc146d.kt
@@ -1,3 +1,4 @@
+import java.util.*
 import kotlin.math.max
 
 fun main() {
@@ -6,27 +7,54 @@ fun main() {
 
 fun abc146d() {
     val n = readLine()!!.toInt()
-    val edge = MutableList(n - 1) { 0 to 0 }
+    val edge = IntArray(n - 1) { 0 }
     val nodes = List(n + 1) { mutableSetOf<Int>() }
+    val edgeOfNode = List(n + 1) { mutableSetOf<Int>() }
     var k = 0
-    repeat(n - 1) {
+    repeat(n - 1) { edgeId ->
         val (a, b) = readLine()!!.split(" ").map { it.toInt() }
-        edge[it] = a to b
         nodes[a].add(b)
         nodes[b].add(a)
+        edgeOfNode[a].add(edgeId)
+        edgeOfNode[b].add(edgeId)
         val max_size = max(nodes[a].size, nodes[b].size)
         k = max(k, max_size)
     }
     val colorSet = (1..k).toSet()
-    val colors = List(n + 1) { mutableSetOf<Int>() }
+    var start = 1
+    while (nodes[start].size != 1) start++
+    val visited = mutableSetOf<Int>()
+    val nextNodes = ArrayDeque<Int>()
+    nextNodes.add(start)
+    while (visited.size < n) {
+        val count = nextNodes.size
+        for (i in 0 until count) {
+            val from = nextNodes.poll()
+            if (from in visited) continue
+            visited.add(from)
+            nextNodes.addAll(nodes[from])
+            val paintEdge = mutableListOf<Int>()
+            var usedColor = 0
+            for (edgeId in edgeOfNode[from]) {
+                if (edge[edgeId] == 0) {
+                    paintEdge.add(edgeId)
+                } else {
+                    usedColor = edge[edgeId]
+                }
+            }
+            for (e in paintEdge) {
+                usedColor = nextColor(usedColor, k)
+                edge[e] = usedColor
+            }
+        }
+    }
     println(k)
-    for (e in edge) {
-        val nodeA = e.first
-        val nodeB = e.second
-        val used = colors[nodeA].union(colors[nodeB])
-        val color = colorSet.minus(used).first()
-        colors[nodeA].add(color)
-        colors[nodeB].add(color)
+    for (color in edge) {
         println(color)
     }
+}
+
+private fun nextColor(current: Int, k: Int): Int {
+    val next = current + 1
+    return if (next > k) 1 else next
 }
