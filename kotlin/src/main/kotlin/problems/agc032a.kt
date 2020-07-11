a2ff5208616628ba8b7a17d27b89a50114998f70
Gen <archiherewego@gmail.com>
2020-07-12 00:23:00 +0900

    Retry AGC032A
    B[i]の位置と値の差を使ってどの値を入れていくかを決定すればいけるんじゃないかと思ったが、どうもそううまくはいかないらしい

--- kotlin/src/main/kotlin/problems/agc032a.kt
+++ kotlin/src/main/kotlin/problems/agc032a.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 // https://atcoder.jp/contests/agc032/tasks/agc032_a
 // TODO: try to AC
 // 雰囲気はわかる、B(i)の値とi番目の関係から、実現が不可能とか、何番目〜何番目の間に挿入が必要とかはわかる
@@ -16,7 +18,32 @@ fun agc032a() {
         println(-1)
         return
     }
-    println(1)
+    val priorities = List(n) { PriorityQueue<Int>() }
+    val ans = IntArray(n) { 0 }
+    for (i in b.indices) {
+        // 0indexなので1足してる(3番めにある3なら最後に挿入しても問題ない、という意味)
+        // つまりこのpriorityとは挿入までの猶予を意味しているともいえそう
+        // この値が大きいということは、それだけ早くに挿入しておかないとならない、ということになる
+        val p = i - b[i]
+        priorities[b[i] - 1].add(-p)
+    }
+    // 最初に挿入するのは必ず1
+    var maxPosition = MaxPosition(0, 0)
+    var priorityValue = Int.MAX_VALUE
+    for (i in 0 until n) {
+        priorityValue = Int.MAX_VALUE
+        for (j in 0..i) {
+            if (priorities[j].isEmpty()) continue
+            val p = priorities[j].peek()
+            if (p < priorityValue) {
+                priorityValue = p
+                maxPosition = MaxPosition(j + 1, j)
+            }
+        }
+        priorities[maxPosition.index].poll()
+        ans[i] = maxPosition.value
+    }
+    println(ans.joinToString("\n"))
 }
 
 private fun isImpossible(values: List<Int>): Boolean {
@@ -25,3 +52,5 @@ private fun isImpossible(values: List<Int>): Boolean {
     }
     return false
 }
+
+private data class MaxPosition(val value: Int, val index: Int)
