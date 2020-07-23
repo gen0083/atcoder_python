fbbeed010555d56aafc39d5f0c3c831da399c710
Gen <archiherewego@gmail.com>
2020-07-24 00:19:04 +0900

    Solve AGC019B with
    Intだとオーバーフローする場合があるので注意
    このオーバーフローする、というのも忘れがちでよくひっかかる

--- kotlin/src/main/kotlin/problems/agc019b.kt
+++ kotlin/src/main/kotlin/problems/agc019b.kt
@@ -1,4 +1,4 @@
-//
+// https://atcoder.jp/contests/agc019/tasks/agc019_b
 
 fun main() {
     agc019b()
@@ -9,7 +9,7 @@ fun agc019b() {
     val n = a.length.toLong()
     var count = (n - 1) * n / 2 + 1
     val appear = a.groupingBy { it }.eachCount()
-    for (value in appear.values) {
+    for (value in appear.values.map { it.toLong() }) {
         count -= (value * (value - 1) / 2)
     }
     println(count)
