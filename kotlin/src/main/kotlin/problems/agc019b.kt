62195afbf0195602f0e94d7c779181adabccf60b
Gen <archiherewego@gmail.com>
2020-07-23 16:47:42 +0900

    Try AGC019B with
    解説を見たが、なぜそうなるのかが理解できない
    また証明の持っていきかたも自分のものにできる気がしない
    結局同じ文字の出現回数から和の公式使って求めた個数だけ同じ結果が得られる、となる理由がわからない
    実際にやったらたしかに選んだi,jの文字が同じなら、同じ反転結果がいくつか出現することは事実っぽいが、だからといってそれが全パターンに当てはまる、という確信が持てないし、なんでそうなるのかも腑に落ちない
    難しい
    
    ただ、全組み合わせパターンは和の公式で求められる、という部分だけは自力でもたどり着けていた
    そこからさらに各文字の出現回数を元に和の公式使って重複するパターンを求める、というのは思考が及ばなかった

--- kotlin/src/main/kotlin/problems/agc019b.kt
+++ kotlin/src/main/kotlin/problems/agc019b.kt
@@ -6,48 +6,11 @@ fun main() {
 
 fun agc019b() {
     val a = readLine()!!
-    val n = a.length - 1
-    var count = (n + 1) * n / 2 + 1
-    var p = ' '
-    var t = 0
-    val s = mutableMapOf<Char, Int>()
-    for (i in 0 until 26) {
-        s[('a' + i).toChar()] = -1
-    }
-    // 同じ文字の連続するパターンをカウント
-    for (i in 0..n) {
-        if (a[i] == p) {
-            t++
-            count -= t
-        } else {
-            t = 0
-            // 同じ文字に囲まれた偶数区間
-            if (s.getOrDefault(a[i], -1) >= 0) {
-                val sand = i - s[a[i]]!! - 1
-                if (sand % 2 == 0) {
-                    count -= (sand / 2)
-                    //                    count -= 1
-                }
-            }
-            s[a[i]] = i
-        }
-        p = a[i]
-    }
-    // ある文字を基準に左右対称となる部分文字列のパターンをカウント
-    for (i in 0..n) {
-        t = 1
-        p = a[i]
-        var sameChar = true
-        while (i - t >= 0 && i + t <= n) {
-            if (a[i - t] != a[i + t]) break
-            if (sameChar && a[i + t] == p) {
-                // 連続する同じ文字列で左右対称はすでにカウントしているのでカウントしない
-            } else {
-                count--
-                sameChar = false
-            }
-            t++
-        }
+    val n = a.length.toLong()
+    var count = (n - 1) * n / 2 + 1
+    val appear = a.groupingBy { it }.eachCount()
+    for (value in appear.values) {
+        count -= (value * (value - 1) / 2)
     }
     println(count)
 }
