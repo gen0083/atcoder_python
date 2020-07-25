b8cfe6007eae11c77148a1c07193df32af90747f
Gen <archiherewego@gmail.com>
2020-07-25 22:59:35 +0900

    Longね・・・
    最初に提出したコードで正解が導けたらしい
    単に答えがIntの範囲に収まらない、とただそれだけのことだった・・・
    まあ答えがIntに収まらない、とは思い至ってない時点でダメなんだろうけどな・・・

--- kotlin/src/main/kotlin/msolutions2020/msolutions2020d.kt
+++ kotlin/src/main/kotlin/msolutions2020/msolutions2020d.kt
@@ -9,38 +9,41 @@ fun main() {
 
 fun msolutions2020d() {
     val n = readLine()!!.toInt()
-    val kabuka = readLine()!!.split(" ").map { it.toInt() }
-    val cash = IntArray(n + 1) { 0 }
-    val kabu = IntArray(n + 1) { 0 }
-    val total = IntArray(n + 1) { 0 }
-    cash[0] = 1000
-    total[0] = 1000
-    cash[1] = cash[0]
-    total[1] = total[0]
-    for (i in 0 until n) {
-        val buy = cash[i + 1] / kabuka[i]
-        val price = kabuka[i] * buy
-        val remain = cash[i + 1] - kabuka[i] * buy
-        if (total[i] > price + remain) {
-            kabu[i + 1] = kabu[i]
-            cash[i + 1] = cash[i]
-            total[i + 1] = total[i]
-        } else {
-            kabu[i + 1] = buy
-            cash[i + 1] = remain
-            total[i + 1] = price + remain
-        }
-        for (j in i + 1 until n) {
-            if (buy * kabuka[j] + remain > total[i + 1]) {
-                kabu[j + 1] = 0
-                cash[j + 1] = buy * kabuka[j] + remain
-                total[j + 1] = cash[j + 1]
+    val a = readLine()!!.split(" ").map { it.toInt() }
+    var soko = Int.MAX_VALUE
+    var yama = Int.MIN_VALUE
+    // 最初に提出したコードで考え方はあってて、ただ答えがIntにおさまらないからオーバーフローでWAになってただけだった
+    // N=80日なので、最大で100->200->100->200...と変動する場合、2^40*1000が答えになる可能性がある
+    // つまりそれはIntでは収まらない
+    // そこに気づいていれば・・・
+    // もっとも、このコードはもっと単純化して、単に前日との比較だけですむ
+    var current = 1000L
+    var searchSoko = true
+    for (value in a) {
+        if (searchSoko) {
+            if (soko > value) {
+                soko = value
             } else {
-                kabu[j + 1] = kabu[i + 1]
-                cash[j + 1] = cash[i + 1]
-                total[j + 1] = total[i + 1]
+                searchSoko = false
+                yama = value
+            }
+        } else {
+            if (yama < value) {
+                yama = value
+            } else {
+                searchSoko = true
+                if (yama > soko) {
+                    val buy = current / soko
+                    current += buy * (yama - soko)
+                }
+                soko = value
+                yama = Int.MIN_VALUE
             }
         }
     }
-    println(total.last())
+    if (yama > soko) {
+        val buy = current / soko
+        current += buy * (yama - soko)
+    }
+    println(current)
 }
