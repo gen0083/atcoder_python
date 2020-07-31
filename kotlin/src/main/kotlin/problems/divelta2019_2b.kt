2b54f377e36fca8aa3be4c31bb4cafc8a9c89031
Gen <archiherewego@gmail.com>
2020-08-01 00:11:29 +0900

    Update divelta2019_2b.kt
    同じ差のパスをmapでまとめて・・・とやってみたが、このやり方だとダメっぽい
    xでソートしてやってもそれが最適とは限らないので
    0->1と大きいノードへのパスしか検討してないからかもしれない

--- kotlin/src/main/kotlin/problems/divelta2019_2b.kt
+++ kotlin/src/main/kotlin/problems/divelta2019_2b.kt
@@ -14,7 +14,42 @@ fun divelta2019_2b() {
         plots.add(x to y)
     }
     val sortX = plots.sortedBy { it.first }
-    val sortY = plots.sortedBy { it.second }
+    val costMap = mutableMapOf<Pair<Long, Long>, MutableList<Pair<Int, Int>>>()
+    for (i in 0 until n) {
+        val base = sortX[i]
+        for (j in i + 1 until n) {
+            val t = sortX[j]
+            val p = t.first - base.first
+            val q = t.second - base.second
+            if (p to q in costMap) {
+                costMap[p to q]!!.add(i to j)
+            } else {
+                costMap[p to q] = mutableListOf(i to j)
+            }
+        }
+    }
+    var cost = 0
+    val visited = mutableSetOf<Int>()
+    for (list in costMap.values) {
+        val lines = mutableSetOf<Int>()
+        for ((i, j) in list) {
+            if (i in visited || j in visited) continue
+            // 同じp,qでも異なる直線かもしれないのでその確認は必要
+            if (i !in lines && j !in lines) {
+                cost++
+            }
+            lines.add(i)
+            lines.add(j)
+        }
+        visited.addAll(lines)
+        if (visited.size == n) break
+    }
+    if (visited.size < n) {
+        // 最後に1つだけ余ったりする、もしくは基点がすでに他のもので使われてしまっている場合の対策場
+        // あぶれたのが複数ある場合、その2点を直線で繋げばコスト1で2点回収できる
+        cost += (n - visited.size) / 2 + 1
+    }
+    println(cost)
 }
 
 private fun testOrder() {
