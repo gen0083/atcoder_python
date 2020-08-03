44b47d912aff856593b8cb17fbd0a09fd7a9c10c
Gen <archiherewego@gmail.com>
2020-08-03 20:20:06 +0900

    Try ABC174E
    考え方はともかく、解き方のイメージが良く分からん

--- kotlin/src/main/kotlin/abc174/abc174e.kt
+++ kotlin/src/main/kotlin/abc174/abc174e.kt
@@ -5,4 +5,20 @@ fun main() {
 }
 
 fun abc174e() {
+    val (n, k) = readLine()!!.split(" ").map { it.toLong() }
+    val a = readLine()!!.split(" ").map { it.toLong() }
+    val counts = LongArray(n.toInt()) { 0L }
+    val total = a.sum()
+    if (total <= k) {
+        println(1)
+        return
+    }
+    var count = k + 1
+    var i = n.toInt() - 1
+    while (count > k) {
+        val t = a[i]
+        for (j in 0 until i) {
+        
+        }
+    }
 }
