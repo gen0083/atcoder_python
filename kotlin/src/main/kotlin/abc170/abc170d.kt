4f1653a36c3257ce4d0b38ddebce3b29f5eda240
Gen <archiherewego@gmail.com>
2020-08-06 15:53:53 +0900

    Update ABC170D
    多少早くはなったが、誤差
    まだ5〜6sかかるので結局ダメ

--- kotlin/src/main/kotlin/abc170/abc170d.kt
+++ kotlin/src/main/kotlin/abc170/abc170d.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 // https://atcoder.jp/contests/abc170/tasks/abc170_d
 
 fun main() {
@@ -8,40 +10,26 @@ fun abc170d() {
     val n = readLine()!!.toInt()
     val a = readLine()!!.split(" ").map { it.toInt() }
     val sorted = a.sorted()
-    var i = 0
     val primes = mutableSetOf<Int>()
-    if (sorted[i] == 1) {
-        // 1が存在する場合、1が単独なら1つだけ、複数あるなら0
-        // 他のあらゆる数は1で割り切れるから
-        if (sorted[i + 1] == 1) {
-            println(0)
-        } else {
-            println(1)
-        }
-        return
-    }
     val sameNum = mutableSetOf<Int>()
-    while (i < n) {
-        val t = sorted[i]
+    val queue = ArrayDeque<Int>(sorted)
+    while (queue.isNotEmpty()) {
+        val t = queue.poll()
         if (t in primes) {
-            // 消してしまったらダメ
             sameNum.add(t)
         } else {
             // prime check
-            var isPrime = true
-            for (e in primes) {
-                if (t % e == 0) {
-                    isPrime = false
-                    break
+            primes.add(t)
+            val len = queue.size
+            repeat(len) {
+                val e = queue.poll()
+                if (t == e) {
+                    sameNum.add(e)
+                } else if (e % t != 0) {
+                    queue.add(e)
                 }
-                // 素数をしらべるならルートまででいいが、この問題に置いてはそれでは足りない
-                // たとえば16の場合、2乗して16になるのは4だが、4がなくとも8がある場合に16は他の数で割り切れる数になる
-                // さりとて2倍で処理すると計算量的に間に合わないので難しい
-                if (e * 2 > t) break
             }
-            if (isPrime) primes.add(t)
         }
-        i++
     }
     println(primes.count() - sameNum.count())
 }
