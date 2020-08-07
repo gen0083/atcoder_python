9451a393b6b5198abfbeda14d367641089785767
Gen <archiherewego@gmail.com>
2020-08-08 01:00:31 +0900

    Solve ABC166E/171C
    171cは提出してなかっただけっぽい
    リアタイでは26進数なのにうまくいかねえって解けなかったやつ
    166EはAi+Aj=|i-j|の式を変形したらよいということがわかっても、これをどう変形するかで答えが変わるのが難しい
    Ai-iとするのかi-Aiとするのかで結果が異なる
    その理由はiより前の位置で一致するものと後ろの位置で一致するものが重複する、すなわち二重でカウントされるものが生じることに原因があるということだろう
    重複を避けるために、i-Aiとすることでi<jのもののみ検討するようにしてるんだろうが、自力でそこまで思い至れるかと言われると難しい
    あとLongで計算すべきところをIntでやってて微妙にWAが出る罠に相変わらずよくハマってるので、いい加減なんとかならんかな

--- kotlin/src/main/kotlin/abc166/abc166e.kt
+++ kotlin/src/main/kotlin/abc166/abc166e.kt
@@ -6,26 +6,25 @@ fun main() {
 
 fun abc166e() {
     val n = readLine()!!.toInt()
-    val height = readLine()!!.split(" ").map { it.toInt() }
-    var count = 0
-    //    for (i in 0 until n) {
-    //        var sa = height[i]
-    //        while(i + sa < n) {
-    //            if (height[i] + height[i + sa] == sa) {
-    //                count++
-    //            }
-    //            sa++
-    //        }
-    //    }
-    val mj = mutableMapOf<Int, Int>()
-    val mi = height.mapIndexed { i, e ->
-        val sa = i - e
-        val c = mj.getOrDefault(sa, 0) + 1
-        mj[sa] = c
-        i + e
+    val height = readLine()!!.split(" ").map { it.toLong() }
+    var count = 0L
+    val plus = mutableMapOf<Long, Long>()
+    val minus = mutableMapOf<Long, Long>()
+    for ((i, v) in height.withIndex()) {
+        // これなんでv - iだとダメなの？
+        // たぶんv-iにしちゃうと、jがi<jの場合とi>jの場合の区別がつかなくなるからだと思う
+        // |i - j| = Ai + Ajだから式変形してAi + i = Aj - jみたいな感じになるんだろうなとは思ったんだが
+        // 差の絶対値をどうやって変形するのかがよくわからなかった
+        // 常にi>jの数のみ考えればよいようにするためにあえてi - Aiとしてるんだと思う
+        // v-iにした場合、すでに数えたやつも同じ値になる可能性があるので重複カウントが生じる可能性がある
+        // それを排除してるんだと思う
+        val p = i.toLong() + 1L + v
+        val m = i.toLong() + 1L - v
+        plus[p] = plus.getOrDefault(p, 0L) + 1L
+        minus[m] = minus.getOrDefault(m, 0L) + 1L
     }
-    for (e in mi) {
-        count += mj.getOrDefault(e, 0)
+    for ((key, value) in plus) {
+        count += (minus.getOrDefault(key, 0L) * value)
     }
     println(count)
 }
