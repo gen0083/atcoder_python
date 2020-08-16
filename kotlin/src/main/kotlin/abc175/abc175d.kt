02f724ccbf9beef56431e008442bdda6188c5f2b
Gen <archiherewego@gmail.com>
2020-08-16 23:36:00 +0900

    Try ABC175D ver2
    とりあえずサンプルテストケースには通るようになった
    ACにはなってない
    コードがかなりややこしいのでデバッグが大変である
    やってることは、各ループグループ（解説で言うサイクル）ごとにkのあまり回数の中で最大となる値を求めて、そこから最大得点を導き出す感じ
    TLEにはなってないがWA

--- kotlin/src/main/kotlin/abc175/abc175d.kt
+++ kotlin/src/main/kotlin/abc175/abc175d.kt
@@ -12,28 +12,52 @@ fun abc175d() {
     val k = ks.toLong()
     val p = readLine()!!.split(" ").map { it.toInt() - 1 }
     val c = readLine()!!.split(" ").map { it.toLong() }
-    var r = (k % n).toInt()
-    if (r == 0) r = n
-    val points = LongArray(n) { 0L }
-    for (i in 0 until n) {
-        var j = p[i]
-        var maxPoint = c[j]
-        var point = 0L
-        repeat(r) {
-            point += c[j]
-            j = p[j]
-            maxPoint = max(maxPoint, point)
+    val loops = mutableListOf<MutableSet<Int>>()
+    val visited = mutableSetOf<Int>()
+    var i = 0
+    while (visited.size < n) {
+        if (i in visited) {
+            i++
+            continue
         }
-        points[i] = maxPoint
+        val set = mutableSetOf<Int>()
+        var j = p[i]
+        while (j !in visited) {
+            set.add(j)
+            visited.add(j)
+            j = p[j]
+        }
+        loops.add(set)
     }
-    var ans = points.max()!!
-    val sum = c.sum()
+    val remain = IntArray(loops.size)
+    val possibleMax = LongArray(loops.size) { Long.MIN_VALUE }
+    for ((i, list) in loops.withIndex()) {
+        var r = (k % list.size).toInt()
+        if (r == 0) r = list.size
+        remain[i] = r
+        for (j in list) {
+            var next = p[j]
+            var maxPoint = c[next]
+            var point = 0L
+            repeat(r) {
+                point += c[next]
+                next = p[next]
+                maxPoint = max(maxPoint, point)
+            }
+            possibleMax[i] = max(possibleMax[i], maxPoint)
+        }
+    }
+    val maxPair = possibleMax.mapIndexed { index, l -> index to l }
+    val max = maxPair.maxBy { it.second }!!
+    var ans = max.second
+    var sum = 0L
+    for (i in loops[max.first]) {
+        sum += c[i]
+    }
     if (sum > 0L) {
-        val count = k / n
+        val count = k / loops[max.first].size
         ans += (sum * count)
+        if (remain[max.first] == loops[max.first].size) ans -= sum
     }
     println(ans)
-    println(29507023469L)
-    println(18904263608)
-    println(1000000000000000000)
 }
