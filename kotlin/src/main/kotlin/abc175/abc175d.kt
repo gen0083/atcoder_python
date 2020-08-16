e659a0f449c38ccdb79291d8d06289e203550ec1
Gen <archiherewego@gmail.com>
2020-08-16 14:18:22 +0900

    Try ABC175D
    要はk回の移動を行うが、実際に調べなければならないのはnで割ったあまりの回数分の移動で、各マスから開始したときに最大何点とれるかがわかればよい
    と考えたが、残念ながらnこの要素の中にループが複数存在する場合がある（入力例4）
    この解き方はループが1通りしかない場合は有効ではあると思われる

--- kotlin/src/main/kotlin/abc175/abc175d.kt
+++ kotlin/src/main/kotlin/abc175/abc175d.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 //
 
 fun main() {
@@ -5,4 +7,33 @@ fun main() {
 }
 
 fun abc175d() {
+    val (ns, ks) = readLine()!!.split(" ")
+    val n = ns.toInt()
+    val k = ks.toLong()
+    val p = readLine()!!.split(" ").map { it.toInt() - 1 }
+    val c = readLine()!!.split(" ").map { it.toLong() }
+    var r = (k % n).toInt()
+    if (r == 0) r = n
+    val points = LongArray(n) { 0L }
+    for (i in 0 until n) {
+        var j = p[i]
+        var maxPoint = c[j]
+        var point = 0L
+        repeat(r) {
+            point += c[j]
+            j = p[j]
+            maxPoint = max(maxPoint, point)
+        }
+        points[i] = maxPoint
+    }
+    var ans = points.max()!!
+    val sum = c.sum()
+    if (sum > 0L) {
+        val count = k / n
+        ans += (sum * count)
+    }
+    println(ans)
+    println(29507023469L)
+    println(18904263608)
+    println(1000000000000000000)
 }
