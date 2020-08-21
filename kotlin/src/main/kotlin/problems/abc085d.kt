27876985ae49a66e5c59d34f05495a9cf4f8b405
Gen <archiherewego@gmail.com>
2020-08-21 23:36:36 +0900

    Update ABC085D
    処理速度的にはPriorityQueue使うのと変わらなかった（微妙に悪い？）
    それだけPriorityQueueの実装がよいってことなんだろうなぁ

--- kotlin/src/main/kotlin/problems/abc085d.kt
+++ kotlin/src/main/kotlin/problems/abc085d.kt
@@ -1,4 +1,4 @@
-import java.util.*
+import java.lang.Long.max
 
 //
 
@@ -10,28 +10,31 @@ fun abc085d() {
     val (ns, hs) = readLine()!!.split(" ")
     val n = ns.toInt()
     var h = hs.toLong()
-    val priorityQueue = PriorityQueue<Katana> { o1, o2 -> (o2.damage - o1.damage).toInt() }
+    var aMax = 0L
+    val thr = mutableListOf<Long>()
     repeat(n) {
         val (a, b) = readLine()!!.split(" ").map { it.toLong() }
-        priorityQueue.add(Katana(a, true))
-        priorityQueue.add(Katana(b, false))
+        aMax = max(aMax, a)
+        thr.add(b)
     }
     var count = 0L
+    val sorted = thr.sortedDescending()
+    var i = 0
     while (h > 0L) {
-        val k = priorityQueue.poll()
-        h -= k.damage
-        count++
-        if (k.repeat) {
-            val r = h / k.damage
-            h -= k.damage * r
+        val k = if (i < n) sorted[i] else 0L
+        if (k > aMax) {
+            h -= k
+            i++
+            count++
+        } else {
+            val r = h / aMax
+            h -= aMax * r
             count += r
             if (h > 0L) {
-                h -= k.damage
+                h -= aMax
                 count++
             }
         }
     }
     println(count)
 }
-
-private data class Katana(val damage: Long, val repeat: Boolean)
