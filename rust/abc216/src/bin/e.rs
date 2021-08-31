c2f42e6292dfd4b65dd8ab89637f2ff4e82d931d
Gen <archiherewego@gmail.com>
2021-08-31 21:14:56 +0900

    try abc216e
    priority queue使えば余裕じゃんと思ったけどTLEだったわ
    確かに1ずつ減らしてまたpqに入れるのは無駄が多いもんなぁ

--- rust/abc216/src/bin/e.rs
+++ rust/abc216/src/bin/e.rs
@@ -1,6 +1,30 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 fn main() {
     input!{
+        n: usize,
+        k: u64,
+        a: [u64; n]
     }
+    let mut heap: BinaryHeap<u64> = BinaryHeap::new();
+    heap.extend(a);
+    let mut presure = 0u64;
+    for i in 0..k {
+        match heap.pop() {
+            Some(p) => {
+                if p > 0 {
+                    presure += p;
+                    heap.push(p - 1);
+                } else {
+                    break;
+                }
+            },
+            None => {
+                break;
+            }
+        }
+    }
+    println!("{}", presure);
 }
