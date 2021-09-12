e4a37d901ef2f27a9302f8775584c0c2f0774d9d
Gen <archiherewego@gmail.com>
2021-09-12 20:39:06 +0900

    make_contigousは1.48.0から
    atcoderは1.42.0だからつかえない

--- rust/abc217/src/bin/e.rs
+++ rust/abc217/src/bin/e.rs
@@ -1,6 +1,30 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
-    input!{
+    input! {
+        q: usize,
+    }
+    let mut queue: VecDeque<u64> = VecDeque::new();
+    let mut is_dirty = true;
+    for _ in 0..q {
+        input! {
+            c: usize,
+        }
+        if c == 2 {
+            println!("{}", queue.pop_front().unwrap());
+        } else if c == 3 {
+            if is_dirty {
+                queue.make_contiguous().sort();
+                is_dirty = false;
+            }
+        } else {
+            input! {
+                x: u64
+            }
+            queue.push_back(x);
+            is_dirty = true;
+        }
     }
 }
