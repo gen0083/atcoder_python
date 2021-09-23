64f8cc2d95875421fb26b2a9a75f24a129102813
Gen <archiherewego@gmail.com>
2021-09-23 19:48:44 +0900

    solve abc217e with ans
    解説みてだからなぁ
    キューとPriorityQueue（RustではBinaryHeap）を用意して処理すればいいってだけ
    基本的にはキューを使った先入れ先出し
    ソートが絡むとキューの中身をheapへ移動
    heapが空でなければそっちから一番小さい値を引っ張り出してればいいし
    新しい要素を後ろに入れる操作はキューに入れていけばいいだけ（heapに値がある間はheapから取り出されるため）
    言われてみれば簡単なんだけどなぁ

--- rust/abc217/src/bin/e.rs
+++ rust/abc217/src/bin/e.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::{BinaryHeap, VecDeque};
 
 use proconio::input;
 
@@ -6,29 +6,33 @@ fn main() {
     input! {
         q: usize,
     }
-    let mut v: Vec<u64> = vec![];
-    let mut is_dirty = true;
-    let mut i = 0usize;
+    let mut queue: VecDeque<i64> = VecDeque::new();
+    let mut heap: BinaryHeap<i64> = BinaryHeap::new();
     for _ in 0..q {
         input! {
             c: usize,
         }
         if c == 2 {
-            println!("{}", v[i]);
-            i+=1;
+            match heap.pop() {
+                Some(i) => {
+                    println!("{}", -i);
+                },
+                None => {
+                    if let Some(j) = queue.pop_front() {
+                        println!("{}", -j);
+                    }
+                }
+            }
         } else if c == 3 {
-            if is_dirty {
-                v = v[i..].to_vec();
-                v.sort();
-                i = 0;
-                is_dirty = false;
+            if !queue.is_empty() {
+                heap.extend(queue.iter());
+                queue.clear();
             }
         } else {
             input! {
-                x: u64
+                x: i64
             }
-            v.push(x);
-            is_dirty = true;
+            queue.push_back(-x);
         }
     }
 }
