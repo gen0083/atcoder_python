ef828969c06ba78f3c964b079cfc0be96cf4a6c8
Gen <archiherewego@gmail.com>
2022-01-30 23:24:59 +0900

    try abc230e
    こういうifを多用するやり方はうまくいかない

--- rust/abc230/src/bin/e.rs
+++ rust/abc230/src/bin/e.rs
@@ -1,6 +1,29 @@
+use std::cmp::max;
+
 use proconio::input;
 
 fn main() {
-    input!{
+    input! {
+        n: u64,
     }
+    let mut l = 2u64;
+    let mut r = n;
+    let mut t = n;
+    let mut f = 1u64;
+    while l < r {
+        let x = n / l;
+        if x > l {
+            t += x;
+        }
+        let mut i = x;
+        while i * l <= n {
+            i += 1;
+        }
+        t += (r - i + 1) * f;
+        f = l;
+        r = i - 1;
+        if l == r {t+= n / l;}
+        l += 1;
+    }
+    println!("{}", t);
 }
