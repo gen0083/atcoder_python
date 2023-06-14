8214def816ee9ba8bb18a356ae2438e9004d4796
Gen <archiherewego@gmail.com>
2023-06-14 21:17:49 +0900

    典型004

---
+++ kotlin/src/main/kotlin/typical90/typical90_004.kt
@@ -0,0 +1,21 @@
+fun main() {
+    typical90_004()
+}
+
+fun typical90_004() {
+    val (h, w) = readLine()!!.split(" ").map { it.toInt() }
+    val row = IntArray(w) { 0 }
+    val column = IntArray(h) { 0 }
+    val table = List(h) { IntArray(w) { 0 } }
+    repeat(h) { c ->
+        for ((r, v) in readLine()!!.split(" ").map { it.toInt() }.withIndex()) {
+            row[r] += v
+            column[c] += v
+            table[c][r] = v
+        }
+    }
+    for (c in 0 until h) {
+
+        println(table[c].zip(row).map { it.second - it.first }.map { it + column[c] }.joinToString(" "))
+    }
+}
