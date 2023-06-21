b85fe3f189ff60215f9f6057dc74e3d69be3d8b9
Gen <archiherewego@gmail.com>
2023-06-21 17:30:10 +0900

    典型009 改良
    TLEになってないものはACになった
    何が違うんだって感じだったけど、変更前は原点とする座標の計算が間違ってたっぽい
    N^3オーダーだとTLEにかわりはなさそう
    同じような問題が出たときに解けるかと言うと解けない（偏角の求め方がわからん）
    あとは偏角ソートして計算量を減らす改良をすれば良さそう

--- kotlin/src/main/kotlin/typical90/typical90_009.kt
+++ kotlin/src/main/kotlin/typical90/typical90_009.kt
@@ -6,33 +6,34 @@ fun main() {
 
 fun typical90_009() {
     val n = readLine()!!.toInt()
-    val p = mutableListOf<Pair<Float, Float>>()
+    val p = mutableListOf<Point>()
     repeat(n) {
-        val (x, y) = readLine()!!.split(" ").map { it.toFloat() }
-        p.add(x to y)
+        val (x, y) = readLine()!!.split(" ").map { it.toDouble() }
+        p.add(Point(x, y))
     }
     var m = 0.0
     for (i in 0 until n) {
+        val base = p[i]
         for (j in 0 until n) {
             if (i == j) continue
+            val f = p[j]
+            val fa = f - base
+            val a = atan2(fa.y, fa.x) * 180.0 / PI
             for (k in 0 until n) {
                 if (k == i || k == j) continue
-                val f = p[i]
-                val base = p[j]
                 val s = p[k]
-                val a = (f.first - base.first to f.second - base.second)
-                val b = (s.first - base.first to s.second - base.second)
-                val c = a.first * b.first + a.second * b.second
-                val d = sqrt(a.first.pow(2).toDouble() + a.second.pow(2).toDouble()) * sqrt(
-                    b.first.pow(2).toDouble() + b.second.pow(
-                        2
-                    ).toDouble()
-                )
-                val theta = acos(c / d) * 180.0 / PI
-                if (theta.isNaN()) continue
-                m = max(m, theta)
+                val sb = s - base
+                val b = atan2(sb.y, sb.x) * 180.0 / PI
+                val arg = min(abs(b - a), 360.0 - abs(b - a))
+                m = max(m, arg)
             }
         }
     }
     println(m)
+}
+
+private data class Point(val x: Double, val y: Double) {
+    operator fun minus(other: Point): Point {
+        return Point(this.x - other.x, this.y - other.y)
+    }
 }
