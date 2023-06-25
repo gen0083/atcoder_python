685ae14f4a30983d3372a44d5406594ae41de952
Gen <archiherewego@gmail.com>
2023-06-26 00:03:09 +0900

    典型012
    Union-Find実装
    再帰関数で定義すればよかった

--- kotlin/src/main/kotlin/typical90/typical90_012.kt
+++ kotlin/src/main/kotlin/typical90/typical90_012.kt
@@ -6,7 +6,7 @@ fun main() {
 
 fun typical90_012() {
     val (h, w) = readLine()!!.split(" ").map { it.toInt() }
-    val table = List(h + 2) { IntArray(w + 2) { -1 } }
+    val table = UnionFind(h, w)
     val q = readLine()!!.toInt()
     var color = 1
     repeat(q) {
@@ -15,33 +15,11 @@ fun typical90_012() {
             // change color
             val a = Cell(query[1], query[2])
             // same color
-            if (table[a.x][a.y] == -1) {
-                val stack = ArrayDeque<Cell>()
-                stack.add(a)
-                while (stack.isNotEmpty()) {
-                    val time = stack.size
-                    for (i in 0 until time) {
-                        val c = stack.pollFirst()
-                        table[c.x][c.y] = color
-                        val list = listOf(
-                                Cell(c.x - 1, c.y),
-                                Cell(c.x + 1, c.y),
-                                Cell(c.x, c.y - 1),
-                                Cell(c.x, c.y + 1)
-                        )
-                        for (t in list) {
-                            if (table[t.x][t.y] == -1) continue
-                            if (table[t.x][t.y] == color) continue
-                            stack.add(t)
-                        }
-                    }
-                }
-                color += 1
-            }
+            table.add(a)
         } else {
             val a = Cell(query[1], query[2])
             val b = Cell(query[3], query[4])
-            if (table[a.x][a.y] == table[b.x][b.y] && table[a.x][a.y] != -1) {
+            if (table.same(a, b)) {
                 println("Yes")
             } else {
                 println("No")
@@ -51,3 +29,60 @@ fun typical90_012() {
 }
 
 private data class Cell(val x: Int, val y: Int)
+
+private class UnionFind(private val row: Int, private val column: Int) {
+    private val parent = IntArray(row * column + 1) { -1 }
+
+    fun add(c: Cell) {
+        val p = convert(c)
+        if (parent[p] != -1) return
+        val list = listOf(
+                Cell(c.x + 1, c.y),
+                Cell(c.x - 1, c.y),
+                Cell(c.x, c.y + 1),
+                Cell(c.x, c.y - 1)
+        )
+        val rooted = ArrayDeque<Int>()
+        for (cell in list) {
+            val po = convert(cell)
+            if (po == -1) continue
+            if (parent[po] == -1) continue
+            rooted.add(root(po))
+        }
+        if (rooted.isEmpty()) {
+            parent[p] = p
+        } else if (rooted.size == 1) {
+            val r = rooted.pollFirst()
+            parent[p] = r
+        } else {
+            val r = rooted.pollFirst()
+            parent[p] = r
+            while (rooted.isNotEmpty()) {
+                val t = rooted.pollFirst()
+                parent[t] = r
+            }
+        }
+    }
+
+    fun root(p: Int): Int {
+        if (parent[p] == -1) return -1
+        if (parent[p] == p) return p
+        val r = root(parent[p])
+        parent[p] = r
+        return r
+    }
+
+    fun same(a: Cell, b: Cell): Boolean {
+        val pa = convert(a)
+        val pb = convert(b)
+        val ra = root(pa)
+        val rb = root(pb)
+        return ra == rb && ra != -1
+    }
+
+    private fun convert(c: Cell): Int {
+        val position = (c.y - 1) * row + c.x
+        if (position < 0 || position >= parent.size) return -1
+        return position
+    }
+}
