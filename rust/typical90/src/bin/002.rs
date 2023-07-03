51537abe210738d19089f24a565ddd59047c61bf
Gen <archiherewego@gmail.com>
2023-07-03 18:34:57 +0900

    典型002　Rustでビット演算

--- rust/typical90/src/bin/002.rs
+++ rust/typical90/src/bin/002.rs
@@ -2,5 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+    }
+    'a: for i in 1..(1 << (n)) {
+        let mut count = 0;
+        let mut a = String::new();
+        for j in (0..n).rev() {
+            let b = 1 << j;
+            if i & b == b {
+                count -= 1;
+                a += ")";
+            } else {
+                count += 1;
+                a += "(";
+            }
+            if count < 0 {
+                continue 'a;
+            }
+        }
+        if count == 0 {
+            println!("{}", a);
+        }
     }
 }
