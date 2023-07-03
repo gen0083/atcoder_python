4d0433202bbf9294c29c2d0b9e8063fec7595cd3
Gen <archiherewego@gmail.com>
2023-07-03 10:54:34 +0900

    Solve ARC163a
    Rustでは普通にString同士も比較できた

--- rust/arc163/src/bin/a.rs
+++ rust/arc163/src/bin/a.rs
@@ -9,37 +9,14 @@ fn main() {
             n: usize,
             s: String,
         }
-        let f = s.chars().nth(0).unwrap();
-        for c in s.chars() {
-            println!("{} {}", f, c);
-            if c > f {
-                println!("Yes");
-                continue 'outer;
-            }
-        }
-        'inner: for i in 1_usize..=n/2 {
+        for i in 1..n {
             let f = &s[..i];
             let t = &s[i..];
-            if f.len() > t.len() {
-                continue;
-            }
-            for j in 0..i {
-                match f.chars().nth(j).unwrap().cmp(&t.chars().nth(j).unwrap()) {
-                    std::cmp::Ordering::Less => {
-                        println!("Yes");
-                        continue 'outer;
-                    },
-                    std::cmp::Ordering::Equal => continue,
-                    std::cmp::Ordering::Greater => {
-                        continue 'inner;
-                    }
-                }
-            }
-            if f.len() < t.len() {
+            if f < t {
                 println!("Yes");
                 continue 'outer;
             }
         }
-        println!("No")
+        println!("No");
     }
 }
