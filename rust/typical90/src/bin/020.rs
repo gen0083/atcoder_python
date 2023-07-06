834e3ca078fee8ef27084de0817e9348fccc8e1d
Gen <archiherewego@gmail.com>
2023-07-06 22:56:08 +0900

    典型020　自力でやるとこれが限界
    数学がわかってないと厳しいなあ
    誤差が出るんでしょと自前実装したけれど、こうじゃなかった

--- rust/typical90/src/bin/020.rs
+++ rust/typical90/src/bin/020.rs
@@ -2,5 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
+        a: u64,
+        b: u64,
+        c: u64,
     }
+    let loga = log(a);
+    let logc = log(c);
+    if loga < b * logc {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
+fn log(mut x: u64) -> u64 {
+    let mut a = 0;
+    while x > 0 {
+        x = x >> 1;
+        a +=1;
+    }
+    return a - 1;
 }
