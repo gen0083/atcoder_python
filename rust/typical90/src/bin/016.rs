91d1af96f9b2d2f546d0d20b2db78d0a1708d67a
Gen <archiherewego@gmail.com>
2023-07-06 22:55:08 +0900

    典型016　自力で解けた

--- rust/typical90/src/bin/016.rs
+++ rust/typical90/src/bin/016.rs
@@ -1,6 +1,26 @@
+use std::cmp::min;
+
 use proconio::input;
 
 fn main() {
     input! {
+        n: i64,
+        A: i64,
+        B: i64,
+        C: i64,
     }
+    let mut m = 9999_i64;
+    for a in 0_i64..=9999 {
+        let r = n - a * A;
+        if r < 0 {
+            continue;
+        }
+        for b in 0_i64..=9999-a {
+            let c = (r - b * B) / C;
+            if c >= 0 && r == b * B + c * C {
+                m = min(m, a + b + c);
+            }
+        }
+    }
+    println!("{}", m);
 }
