c21f25a735fdd5e6112200e5fc5c389b288ab230
Gen <archiherewego@gmail.com>
2023-07-07 21:51:06 +0900

    典型019　区間DP添字の動きがわけわかめ

--- rust/typical90/src/bin/019.rs
+++ rust/typical90/src/bin/019.rs
@@ -1,6 +1,23 @@
+use std::cmp::min;
+
 use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [i32; 2 * n],
     }
+    let mut dp = vec![vec![i32::MAX; 2 * n + 1]; 2 * n + 1];
+    // まず隣り合うところのコストを予め計算してしまう
+    for i in 0..2 * n - 1 {
+        dp[i + 1][i + 2] = (a[i] - a[i + 1]).abs();
+    }
+    for k in 1..n {
+        for i in (1..2 * (n - k)) {
+            let j = i + 2 * k + 1;
+            dp[i][j] = min(dp[i][j], dp[i][i + 2 * k - 1] + dp[i + 2 * k][j]);
+            dp[i][j] = min(dp[i][j], dp[i + 1][j - 1] + (a[i - 1] - a[j - 1]).abs());
+        }
+    }
+    println!("{}", dp[1][2 * n]);
 }
