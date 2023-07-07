3948f23a45f837efe615c2604cceaed2c58e2637
Gen <archiherewego@gmail.com>
2023-07-07 22:51:17 +0900

    典型004をRustで
    数値を文字列にして出力するほうが難しかった
    今回のは計算が必要だったのでややこしい処理になっているが、数列を文字列として結合して出力するだけでもややこしい変換が必要
    もっとシンプルに書く方法もあるかも

--- rust/typical90/src/bin/004.rs
+++ rust/typical90/src/bin/004.rs
@@ -1,6 +1,22 @@
+use std::vec;
+
 use proconio::input;
 
 fn main() {
     input! {
+        h: usize,
+        w: usize,
+        a: [[i32; w]; h],
+    }
+    let mut r = vec![0; w];
+    let mut c = vec![0; h];
+    for i in 0..h {
+        for j in 0..w {
+            r[j] += a[i][j];
+            c[i] += a[i][j];
+        }
+    }
+    for i in 0..h {
+        println!("{}", r.iter().enumerate().map(|(j, v)| (v + c[i] - a[i][j]).to_string()).collect::<Vec<_>>().join(" "));
     }
 }
