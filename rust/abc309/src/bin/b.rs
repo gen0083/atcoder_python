8c97dfd021255df15506f2fdd8059ec5d6aef999
Gen <archiherewego@gmail.com>
2023-07-09 12:19:59 +0900

    solve abc309b
    めっちゃ力技で解いた
    もっと楽な方法というかスマートな方法はないものか・・・
    個人的に最後のchar配列を文字列にして出力する部分が気に食わない

--- rust/abc309/src/bin/b.rs
+++ rust/abc309/src/bin/b.rs
@@ -1,16 +1,49 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input!{
+    input! {
         n: usize,
-        a: [[u8;n]; n],
+        a: [Chars; n],
     }
-    let mut b = vec![vec![0_u8;n];n];
+    let mut b = vec![vec![' '; n]; n];
+    let mut r = VecDeque::<char>::new();
     for i in 0..n {
         for j in 0..n {
-            if i == 0 || j == 0 || i == n-1 || j==n-1{
-                // rotate
-            }
+            b[i][j] = a[i][j];
         }
+    }
+    for i in 0..n {
+        r.push_back(a[0][i]);
+    }
+    for i in 1..n {
+        r.push_back(a[i][n - 1]);
+    }
+    for i in (0..n - 1).rev() {
+        r.push_back(a[n - 1][i]);
+    }
+    for i in (1..n - 1).rev() {
+        r.push_back(a[i][0]);
+    }
+    r.rotate_right(1);
+    for i in 0..n {
+        b[0][i] = r.pop_front().unwrap();
+    }
+    for i in 1..n {
+        b[i][n - 1] = r.pop_front().unwrap();
+    }
+    for i in (0..n - 1).rev() {
+        b[n - 1][i] = r.pop_front().unwrap();
+    }
+    for i in (1..n - 1).rev() {
+        b[i][0] = r.pop_front().unwrap();
+    }
+    for i in 0..n {
+        let mut res = String::new();
+        for j in 0..n {
+            res += b[i][j].to_string().as_str();
+        }
+        println!("{}", res);
     }
 }
