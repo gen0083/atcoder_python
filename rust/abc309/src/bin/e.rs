12384cef585d9e2ae2274704abe2e90a077a6082
Gen <archiherewego@gmail.com>
2023-07-10 22:59:13 +0900

    典型007をRustであ
    rustはindex操作にも気を使うなぁ
    usizeだから計算でマイナスになったらpanic起こすとか・・・
    Kotlinでもiのチェックやってたかなあ
    自前で二分探索実装してたから不要だったわ・・・

--- rust/abc309/src/bin/e.rs
+++ rust/abc309/src/bin/e.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
     input!{
+        n: usize,
+        m: usize,
+        p: [usize; n-1],
+        mut h: [(usize, usize); m],
     }
+    h.sort();
+    let mut x = 1_usize;
+    let mut y = 0_usize;
+    for (parent, gen) in h {
+        if x != parent {
+            x = parent;
+            
+        }
+    }
+
 }
--- rust/typical90/src/bin/007.rs
+++ rust/typical90/src/bin/007.rs
@@ -1,6 +1,30 @@
+use std::cmp::min;
+
 use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        mut a: [i64; n],
+        q: usize,
+        b: [i64; q],
+    }
+    a.sort();
+    for bj in b {
+        match a.binary_search(&bj) {
+            Ok(_) => println!("0"),
+            Err(i) => {
+                let mut ans = 1000000000_i64;
+                if i > a.len() - 1 {
+                    ans = min(ans, (a.last().unwrap() - bj).abs());
+                } else {
+                    match i.checked_sub(1) {
+                        Some(j) => ans = min((a[i] - bj).abs(), (a[j] - bj).abs()),
+                        None => ans = a[i] - bj.abs(),
+                    };
+                }
+                println!("{}", ans);
+            }
+        }
     }
 }
