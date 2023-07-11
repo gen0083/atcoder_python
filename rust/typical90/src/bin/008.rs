ecf26cc244bc9d2f0a13e866b9e2558e800b00b8
Gen <archiherewego@gmail.com>
2023-07-11 12:19:19 +0900

    典型008をRustで
    let-else節は1.65からという知見を得た
    AtCoderは使ってる言語バージョンが古すぎるからアップデートを適宜やるようにしろ・・・

--- rust/typical90/src/bin/008.rs
+++ rust/typical90/src/bin/008.rs
@@ -1,6 +1,27 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
+        n: usize,
+        s: Chars,
     }
+    let m = 1000000007;
+    let t: HashMap<char, usize> = "atcoder".chars().enumerate().map(|(i, c)| (c, i)).collect();
+    let mut dp = vec![vec![0; 7]; n + 1];
+    for (i, c) in s.iter().enumerate() {
+        for j in 0..7 {
+            dp[i + 1][j] = dp[i][j];
+        }
+        if let Some(&ci) = t.get(c) {
+            if ci == 0 {
+                dp[i + 1][ci] += 1;
+            } else {
+                dp[i + 1][ci] += dp[i][ci - 1];
+            }
+            dp[i + 1][ci] %= m;
+        }
+    }
+    println!("{}", dp[n][6]);
 }
