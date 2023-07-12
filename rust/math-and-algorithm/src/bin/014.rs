9b693e85e79160c8d5ff7d8b2e2e8f433f948917
Gen <archiherewego@gmail.com>
2023-07-13 01:20:26 +0900

    アルゴリズムと数学014
    素数のセットを出力するやつ

--- rust/math-and-algorithm/src/bin/014.rs
+++ rust/math-and-algorithm/src/bin/014.rs
@@ -1,6 +1,22 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input!{
+        n: i64,
     }
+    let mut i = 2_i64;
+    let mut t = n;
+    let mut a = vec![];
+    while i * i <= t {
+        while t % i == 0 {
+            a.push(i);
+            t /= i;
+        }
+        i += 1;
+    }
+    if t != 1 {
+        a.push(t);
+    }
+    println!("{}", a.iter().map(|i| i.to_string()).join(" "));
 }
