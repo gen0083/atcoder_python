ad4398ea7e52c6f9331a58db9fedacad5c9c102f
Gen <archiherewego@gmail.com>
2023-07-13 13:44:25 +0900

    アルゴリズムと数学016
    桁には気をつけましょう

--- rust/math-and-algorithm/src/bin/016.rs
+++ rust/math-and-algorithm/src/bin/016.rs
@@ -2,5 +2,22 @@ use proconio::input;
 
 fn main() {
     input!{
+        n: usize,
+        a: [i64; n],
     }
+    let mut b = a[0];
+    for &i in a[1..].iter() {
+        let mut c = i;
+        while b >= 1 && c >= 1 {
+            if b > c {
+                b = b % c;
+            } else {
+                c = c % b;
+            }
+        }
+        if c >= 1 {
+            b = c;
+        }
+    }
+    println!("{}", b);
 }
