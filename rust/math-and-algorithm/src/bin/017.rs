5d3f94dd303f8310b1bfc23953678f4a31c81fec
Gen <archiherewego@gmail.com>
2023-07-13 21:23:01 +0900

    説明せずいきなりlcmとか出てきても困る
    数学わかっている人からすれば自明なのかもしれないが、そうではない人にとっては最小公倍数何？ってなる
    最小公倍数自体は知っていても、どういう関係なのかわからない
    コード例を見てやったけど、やっぱり理解できない
    AtCoderの解説も解説になってないと感じるし、こういうのばっかりあると所詮俺とは違う世界なんだなと思ってしまう
    門前払い食らってる感じ？
    まあ数学がわかってないから門前払いなのはしょうがないが・・・

--- rust/math-and-algorithm/src/bin/017.rs
+++ rust/math-and-algorithm/src/bin/017.rs
@@ -2,5 +2,31 @@ use proconio::input;
 
 fn main() {
     input!{
+        n: usize,
+        a: [i128; n],
     }
+    let mut l = lcm(a[0], a[1]);
+    for &i in a[2..].iter() {
+        // 1個ずつLCMを求めて計算する
+        l = lcm(l, i);
+    }
+    println!("{}", l);
+}
+
+fn gcd(mut a: i128, mut b: i128) -> i128 {
+    while a >= 1 && b >= 1 {
+        if a > b {
+            a = a % b;
+        } else {
+            b = b % a;
+        }
+    }
+    if b >= 1 {
+        a = b;
+    }
+    return a;
+}
+
+fn lcm(a: i128, b: i128) -> i128 {
+    return a * b / gcd(a, b);
 }
